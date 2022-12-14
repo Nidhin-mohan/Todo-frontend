575aee35f1b3ba13a9f6ee33474f10f3480ca00e
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2022-12-14 10:17:59 +0530

    moved axios and usestate from tododisplay to app and passing it as props

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import './App.css';
 import Header from './Components/Header';
 import TodoDispay from './Components/TodosDispay';
 import TodoForm from './Components/TodoForm';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const BASE_URL = "http://localhost:4000/todo";
 
 
 function App() {
+    const [todos, setTodos] = useState("");
+
+    const fetchTodosData = async () => {
+      const resp = await axios.get(` ${BASE_URL}/getTodos`);
+
+      setTodos(resp.data.todos);
+    };
+
+    useEffect(() => {
+      fetchTodosData();
+      console.log("useeffect calling fetch data");
+    }, []);
+
   return (
     <div className="App" class="text-gray-400 bg-gray-900 body-font">
       <Header />
       <TodoForm />
-      <TodoDispay />
+      <TodoDispay 
+      todos ={todos}  
+      />
     </div>
   );
 }
--- src/Components/TodosDispay.js
+++ src/Components/TodosDispay.js
@@ -3,22 +3,10 @@ import axios from "axios";
 import Todolist from './Todolist';
 import { useEffect, useState } from "react";
 
-const TodoDispay = () => {
-   const [todos, setTodos] = useState("");
 
-     const fetchUsersData = async () => {
-       const resp = await axios.get(`http://localhost:4000/todo/getTodos`);
 
-       setTodos(resp.data.todos);
-     };
-
-  
-
-   useEffect(() => {
-     fetchUsersData();
-     console.log("useeffect calling fetch data")
-   }, []);
-
+const TodoDispay = ({todos}) => {
+ 
 
   return (
     <div>
